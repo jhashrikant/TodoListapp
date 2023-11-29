d9ab47f5ce17893ab803a77403a88502df97f6e0
Shrikant Jha <87967393+jhashrikant@users.noreply.github.com>
2023-11-29 09:58:52 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+	// const [tasks, setTasks] = useState(() => {
+	// 	const storedTasks = localStorage.getItem('Tasks');
+	// 	return storedTasks ? JSON.parse(storedTasks) : [];
+	// });
+
+
+	const [tasks, setTasks] = useState([]);
+	const [taskValue, setTaskValue] = useState('')
+	const [isEditing, setIsediting] = useState(false)
+	const [editingvalue, setEditingvalue] = useState(null);
+
+	// const date = new Date();
+	// console.log(typeof date)
+
+	// useEffect(() => {
+	// 	localStorage.setItem('Tasks', JSON.stringify(tasks))
+	// }, [tasks])
+
+
+	// function calculateinitialstate() {
+	// 	const storedtask = localStorage.getItem('Tasks');
+	// 	if (storedtask) {
+	// 		return JSON.parse(storedtask)
+	// 	}
+	// 	else {
+	// 		return []
+	// 	}
+	// }
+
+
+	function handleChange(event) {
+		setTaskValue(event.target.value);
+	}
+
+	function handleAddtask() {
+		if (isEditing) {
+			const index = tasks.indexOf(editingvalue)
+			// const editedlist = [...tasks];
+			// editedlist[index] = taskValue
+			// setTasks(editedlist)
+			const updatedtask = [...tasks];
+			updatedtask.splice(index, 1, taskValue)
+			setTasks(updatedtask)
+			// localStorage.setItem( 'Tasks', tasks )
+			// setTasks((prevtask) => {
+			// 	return [...prevtask, prevtask.splice(index, 1, taskValue)]
+			// });
+		}
+		else {
+			if (taskValue !== '') {
+				setTasks((prevtask) => [taskValue, ...prevtask]);
+			}
+		}
+		//push the value in the form field to the tasks aray
+		setTaskValue('');
+		setIsediting(false)
+	}
+
+
+
+	function handleDelete(task) {
+		const updatedtaskList = tasks.filter((currentelement) => currentelement !== task)
+		console.log(updatedtaskList);
+		setTasks(updatedtaskList);
+		// localStorage.removeItem('Tasks',task);
+	}
+
+
+	function handleEdit(task) {
+		setIsediting(true)
+		setTaskValue(task)
+		document.querySelector('.inputText').focus();
+		let currentlyEditingValue = tasks.find((element) => element === task);
+		setEditingvalue(currentlyEditingValue);
+	}
+
+	function handlekeyPress(event) {
+		if (event.key === 'Enter') {
+			handleAddtask()
+		}
+	}
+
+	// const today = new Date();
+	// const formattedDate = today.toDateString();
+
+
+	return (
+		<>
+			<h1 className='heading'>Todo App</h1>
+
+			<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+				<input type='text' value={taskValue} onChange={handleChange} onKeyDown={handlekeyPress} className='inputText' placeholder='Enter your Task' />
+				<button onClick={handleAddtask} className='addTask'>{isEditing ? "save" : 'Addtask'}</button>
+			</div>
+			<ul className='TodoListitemsContainer'>
+				{tasks && tasks.map((task, index) => {
+					return <li key={index} className='Singletodoitem'>
+						{task}
+						<span >
+							<button onClick={() => handleEdit(task)} className='edit'>Edit</button>
+							<button onClick={() => handleDelete(task)} className='delete'>Delete</button>
+						</span>
+					</li>
+				})
+				}
+			</ul >
+		</>
+	)
 }
 
 export default App;
